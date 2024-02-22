0d8247322593651af50f6d10e07214c7dd03e722
Eliza A <elizaveta.devel@gmail.com>
2024-02-22 19:15:08 +0800

    cleanup chatView

--- Views/ChatView.swift
+++ Views/ChatView.swift
@@ -19,44 +19,40 @@ struct ChatView: View {
     //MARK: - Body
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                ScrollView {
-                    ScrollViewReader { proxy in
-                        LazyVStack {
-                            ForEach(viewModel.messages, id: \.id) { message in
-                                ChatBubble(direction: message.isOutgoing ? .right : .left) {
-                                    ChatMessageContent(message: message)
-                                }
-                            }
-                        }
-                        .onChange(of: viewModel.messages) {
-                            if let lastMessage = viewModel.messages.last {
-                                withAnimation {
-                                    proxy.scrollTo(lastMessage.id, anchor: .bottom)
-                                }
-                            }
-                        }
-                        .onAppear() {
-                            if let lastMessage = viewModel.messages.last {
-                                proxy.scrollTo(lastMessage.id, anchor: .bottom)
-                            }
+        ScrollView {
+            ScrollViewReader { proxy in
+                LazyVStack {
+                    ForEach(viewModel.messages, id: \.id) { message in
+                        ChatBubble(direction: message.isOutgoing ? .right : .left) {
+                            ChatMessageContent(message: message)
                         }
                     }
                 }
-
-                HStack {
-                    TextField("Type a message", text: $newMessageText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    Button(action: {
-                        self.sendMessage()
-                    }) {
-                        Text("Send")
+                .onChange(of: viewModel.messages) {
+                    if let lastMessage = viewModel.messages.last {
+                        withAnimation {
+                            proxy.scrollTo(lastMessage.id, anchor: .bottom)
+                        }
                     }
                 }
-                .padding()
+                .onAppear() {
+                    if let lastMessage = viewModel.messages.last {
+                        proxy.scrollTo(lastMessage.id, anchor: .bottom)
+                    }
+                }
             }
         }
+        
+        HStack {
+            TextField("Type a message", text: $newMessageText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: {
+                self.sendMessage()
+            }) {
+                Text("Send")
+            }
+        }
+        .padding()
     }
 
     func sendMessage() {
@@ -65,4 +61,8 @@ struct ChatView: View {
         viewModel.sendMessage(text: newMessageText)
         newMessageText = ""
     }
+}
+
+#Preview {
+    ChatView(viewModel: ChatViewModel())
 }
