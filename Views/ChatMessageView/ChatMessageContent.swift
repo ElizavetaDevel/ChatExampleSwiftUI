fd4333d7f98dcd3810b71c7c12582d1404c2a8fe
Eliza A <elizaveta.devel@gmail.com>
2024-02-22 19:19:06 +0800

    return back MessageContent Previews

--- Views/ChatMessageView/ChatMessageContent.swift
+++ Views/ChatMessageView/ChatMessageContent.swift
@@ -49,3 +49,12 @@ struct ChatMessageContent: View {
         }
     }
 }
+
+#Preview {
+    VStack {
+        ChatMessageContent(message: ChatMessage(text: "Lorem ipsum", isOutgoing: true, senderName: "ME", timestamp: Date()))
+        ChatMessageContent(message: ChatMessage(text: "L", isOutgoing: true, senderName: "ME", timestamp: Date()))
+        ChatMessageContent(message: ChatMessage(text: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", isOutgoing: false, senderName: "Alice", timestamp: Date()))
+    }
+}
+
